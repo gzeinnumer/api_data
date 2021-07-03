ee5c3e7cecf4aa3ea02315b861fa33ba7098b398
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-07-03 15:02:40 +0700

    commit

---
+++ simple_crud_laravel_json_2.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jul 2021 pada 10.01
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simple_crud_laravel_json_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data`
+--
+
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `no_telepon` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data`
+--
+
+INSERT INTO `data` (`id`, `nama`, `no_telepon`, `created_at`, `updated_at`) VALUES
+(1, 'M. Fadli Zein', '082385804045', '2021-07-03 07:46:24', '2021-07-03 07:14:36'),
+(2, 'Zein', '082385804045', '2021-07-03 07:46:35', '2021-07-03 07:14:36');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
